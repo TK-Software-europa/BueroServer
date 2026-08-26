c2d13b061b3bfecb732f6b5a09b4a8cb055198c3
TK-Software-europa <tk-software@outlook.de>
2026-08-26 17:54:48 +0200

    Change server listen address and log message

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Büro-Server läuft auf Port ${PORT}`);
 });
